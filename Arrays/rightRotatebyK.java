ea28e1d7433ca214972c5b08685fabdfe6d97731
Yashraj Kumar Gupta <67089868+Yashraj1142@users.noreply.github.com>
2025-09-14 19:48:47 +0530

    Array

--- Arrays/rightRotatebyK.java
+++ Arrays/rightRotatebyK.java
@@ -1,15 +1,17 @@
-public class leftRotatebyK {
+public class rightRotatebyK {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
         int[] newarr = new int[arr.length];
         int k=3;
 
+        k = k%(arr.length);
+
         for(int i=0;i<k;i++){
-            newarr[arr.length-k+i] = arr[i];
+            newarr[i] = arr[arr.length-k+i];
         }
 
-        for(int i=0;i<arr.length-k;i++){
-            newarr[i] = arr[i+k];
+        for(int i=k;i<arr.length;i++){
+            newarr[i] = arr[i-k];
         }
 
 
