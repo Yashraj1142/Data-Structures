563aec4a2b2ef15dd59d992b044666cf41613bd7
Yashraj Kumar Gupta <67089868+Yashraj1142@users.noreply.github.com>
2026-01-06 14:49:31 +0530

    Strings

---
+++ Strings/LongCommonPrefix.java
@@ -0,0 +1,27 @@
+class LongCommonPrefixSolution{
+    public String commonPrefix(String[] str){
+        String prefix = str[0];
+
+        for(int i=1;i<str.length;i++){
+            while(!str[i].startsWith(prefix)){
+                prefix = prefix.substring(0, prefix.length()-1);    //shrinking the prefix
+
+                if(prefix.length()==0){
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+        
+    }
+}
+
+public class LongCommonPrefix {
+    public static void main(String[] args) {
+        LongCommonPrefixSolution l1 = new LongCommonPrefixSolution();
+
+        String[] str = {"flow", "flight", "flower"};
+        System.out.println(l1.commonPrefix(str));
+    }
+}
