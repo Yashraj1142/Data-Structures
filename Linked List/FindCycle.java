3773392862766103bb6c19f372968cd00c64f877
Yashraj Kumar Gupta <67089868+Yashraj1142@users.noreply.github.com>
2026-01-28 19:23:12 +0530

    Linked List

--- Linked List/FindCycle.java
+++ Linked List/FindCycle.java
@@ -13,6 +13,7 @@ class FindCycleSolution{
         ListNode slow = head;
         ListNode fast = head;
 
+        //finding cycle
         while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
