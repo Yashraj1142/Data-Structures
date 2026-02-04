b401d4bb757ec07d6744784f333af327d93fe604
Yashraj Kumar Gupta <67089868+Yashraj1142@users.noreply.github.com>
2026-02-04 10:47:04 +0530

    Stack

---
+++ Stacks and Queues/StackArray.java
@@ -0,0 +1,74 @@
+class Stack{
+    private int[] stack;
+    private int capacity;
+    private int top;
+
+    //constructor
+    Stack(int size){
+        capacity = size;
+        stack = new int[capacity];
+        top = -1;
+    }
+
+    //push operation
+    void push(int value){
+        if(isFull()){
+            System.out.println("Stack is full");
+            return;
+        }
+
+        stack[++top] = value;
+        System.out.println(value + " pushed");
+    }
+
+    //pop operation
+    int pop(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return stack[top--];
+    }
+
+    //peek operation
+    int peek(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return stack[top];
+    }
+
+    //check if full or not
+    boolean isFull(){
+        return top == capacity-1;
+    }
+
+    //check if empty
+    boolean isEmpty(){
+        return top == -1;
+    }
+}
+
+public class StackArray {
+    public static void main(String[] args) {
+        Stack s1 = new Stack(5);
+
+        //check if empty and full
+        if(s1.isEmpty()) System.out.println("Stack is Empty");
+        if(s1.isFull()) System.out.println("Stack is Full");
+
+        //push elements
+        s1.push(1);
+        s1.push(2);
+        s1.push(3);
+        s1.push(4);
+        s1.push(5);
+
+        //pop
+        System.out.println("pop: " + s1.pop());
+
+        //peek
+        System.out.println("peek: " + s1.peek());
+    }
+}
